6e0f3d3a6a8ed459cc7679cf603f42414642c587
Yash <yashshah@gmail.com>
2024-07-21 20:31:40 +0530

    Backend Completed

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -73,6 +73,45 @@ export const postAppointment = catchAsyncErrors(async(req,res,next)=>{
         success:true,
         message: "Appointment Sent Successfully!",
         appointment,
-    })
+    });
 
-})
+});
+
+export const getAllAppointments =catchAsyncErrors(async(req,res,next)=>{
+    const appointments = await  Appointment.find();
+    res.status(200).json({
+        success: true,
+        appointments,
+    });
+});
+
+export const updateAppointmentStatus = catchAsyncErrors(
+    async (req, res, next) => {
+      const { id } = req.params;
+      let appointment = await Appointment.findById(id);
+      if (!appointment) {
+        return next(new ErrorHandler("Appointment not found!", 404));
+      }
+      appointment = await Appointment.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+      });
+      res.status(200).json({
+        success: true,
+        message: "Appointment Status Updated!",
+      });
+    }
+  );
+export const deleteAppointment= catchAsyncErrors(async(req,res,next)=>{
+    const {id} =req.params;
+     let appointment = await Appointment.findById(id);
+     if(!appointment){
+        return next(new ErrorHandler("Appointment Not Found!",404));
+     }
+     await appointment.deleteOne();
+     res.status(200).json({
+        success: true,
+        message: "Appointment Deleted!",
+     });
+});
