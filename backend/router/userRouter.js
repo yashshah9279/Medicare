33e380fee34257d06a86ba6ebaf93eab9e705b94
Yash <yashshah@gmail.com>
2024-07-21 14:20:19 +0530

    Authorization and Authetication Done

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { addNewAdmin, login, patientRegister } from "../controller/userController.js";
+import { isAdminAuthenticated } from "../middlewares/auth.js";
 
 const router= express.Router();
 
 router.post("/patient/register",patientRegister)
 router.post("/login",login)
-router.post("/admin/addnew",addNewAdmin);
+router.post("/admin/addnew",isAdminAuthenticated,addNewAdmin);
 
 
 export default router;
