36d7ce27e1d73b1b61ce22dd67d7f00f48dcbbbd
Yash <yashshah@gmail.com>
2024-07-21 18:25:39 +0530

    Appointment Booking completed

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,9 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
 import messageRouter from "./router/messageRouter.js";
-import {errorMiddleware} from './middlewares/errorMiddleware.js'
-import userRouter from "./router/userRouter.js"
+import {errorMiddleware} from './middlewares/errorMiddleware.js';
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
 
 const app=express();
 config({path: "./config/config.env"});
@@ -29,6 +30,7 @@ app.use(fileUpload({
 );
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
 dbConnection();
 
 
--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -13,4 +13,12 @@ export const sendMessage = catchAsyncErrors(async(req,res,next)=>{
             message: "Message Sent Successfully",
         });
  
-})
+}); 
+
+export const getAllMessages =catchAsyncErrors(async(req,res,next)=>{
+    const messages =await Message.find();
+    res.status(200).json({
+        success: true,
+        messages,
+    });
+});
