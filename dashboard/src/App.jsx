94fbee5e05c8d35b84bde381f80f38b007fb50a2
Yash <yashshah@gmail.com>
2024-07-22 20:23:32 +0530

    Link changes for deployment

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "https://medicare-0sfm.onrender.com/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/admin/addnew",
+          "https://medicare-0sfm.onrender.com/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone, aadhar, dob, gender, password },
           {
             withCredentials: true,
